e9511dcf371ca934a75f6ee019f7bde6a7c52c2c
Ariana Tzuc <202100185@upqroo.edu.mx>
2023-03-16 11:36:39 -0500

    rep 2

---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <form>
+        <div>
+            <h1>INICIA SESION</h1>
+        </div>
+
+        <div>
+            <div>
+                <Label>Correo</Label>
+            </div>
+            <div>
+                <input placeholder="Alguien@correo"><br>
+            </div>
+            <div>
+                <label>Contraseña</label>
+            </div>
+            <div>               
+                <input type="Password">
+            </div>
+            </div>
+        <div>
+            <Button>INICIAR SESION </Button>
+        </div>
+
+        <!-- <div>
+            <Label>Correo</Label>
+            <input placeholder="Alguien@correo">
+        </div>
+        <div>
+            <label>Contraseña</label>
+            <input type="Password">
+        </div> -->
+    </form>
+</template>
